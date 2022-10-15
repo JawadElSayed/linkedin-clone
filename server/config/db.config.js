7578bc03d3498a6f60856e398fef65394684ddd3
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-15 23:45:48 +0300

    backend > fixing database linking

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const db = () => {
     mongoose
         .connect(
-            "mongodb+srv://jawad:JawaD-991@cluster0.uabpnpd.mongodb.net/?retryWrites=true&w=majority"
+            "mongodb+srv://jawad:JawaD-991@cluster0.uabpnpd.mongodb.net/linkedindb?retryWrites=true&w=majority"
         )
         .then(() => {
             console.log("Database Connected");
