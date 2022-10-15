1e198bbf56e899f03473beae6647d8f8c61bdf67
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-16 00:56:02 +0300

    backend > creating login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../Models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const signup = async (req, res) => {
     const { name, email, password, major, user_type } = req.body;
@@ -21,6 +22,35 @@ const signup = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    // check input
+    if (!user)
+        return res.status(401).json({
+            status: "error",
+            message: "incorrect email or password",
+        });
+
+    // checking password
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch)
+        return res.status(401).json({
+            status: "error",
+            message: "incorrect email or password",
+        });
+
+    const token = jwt.sign(
+        { email: user.email, name: user.name, user_type: user.user_type },
+        process.env.JWT_SECRET_KEY,
+        {
+            expiresIn: "1h",
+        }
+    );
+    res.status(200).json(token);
+};
+
 module.exports = {
     signup,
+    login,
 };
