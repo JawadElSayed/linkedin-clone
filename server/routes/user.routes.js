06b12ee30964294471199e09114c3ed66d456c0e
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-16 19:16:47 +0300

    backend > adding search job route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { editProfile, getAllJobs, getJobById } = require("../controllers/user.controller");
+const { editProfile, getAllJobs, getJobById, searchForJob } = require("../controllers/user.controller");
 const router = Router();
 
 router.post("/edit_profile", editProfile);
 router.get("/jobs", getAllJobs);
 router.get("/job/:id", getJobById);
+router.get("/search/:search", searchForJob);
 
 module.exports = router;
