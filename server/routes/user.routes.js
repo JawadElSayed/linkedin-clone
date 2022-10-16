99f263b4d6bbcb0d79aebca673f9e676b2bc901a
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-16 23:07:24 +0300

    backend > adding apply route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
-const { editProfile, getAllJobs, getJobById, searchForJob } = require("../controllers/user.controller");
+const {
+    editProfile,
+    getAllJobs,
+    getJobById,
+    searchForJob,
+    apply,
+} = require("../controllers/user.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const router = Router();
 
-router.post("/edit_profile", editProfile);
-router.get("/jobs", getAllJobs);
-router.get("/job/:id", getJobById);
-router.get("/search/:search", searchForJob);
+router.post("/edit_profile", authMiddleware, editProfile);
+router.get("/jobs", authMiddleware, getAllJobs);
+router.get("/job/:id", authMiddleware, getJobById);
+router.get("/search/:search", authMiddleware, searchForJob);
+router.post("/apply", authMiddleware, apply);
 
 module.exports = router;
