5c514264bce4fca38de8b3d9280a5b3bedf26b3e
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-16 17:40:08 +0300

    backend > adding get jobs route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const { editProfile } = require("../controllers/user.controller");
+const { editProfile, getAllJobs } = require("../controllers/user.controller");
 const router = Router();
 
 router.post("/edit_profile", editProfile);
+router.get("/jobs", getAllJobs);
 
 module.exports = router;
