5cd66a8c05f6a3c81378ecb47a93d4e03be9d5e4
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-17 02:52:08 +0300

    backend > modifying auth middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -7,8 +7,6 @@ const authMiddleware = async (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.email = decoded.email;
-        const user = await User.findOne({ email: decoded.email }).lean();
-        req.user = { ...user };
         next();
     } catch (err) {
         return res.status(401).json({
