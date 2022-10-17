65f7a086b6c18c5a809924a83677522c53cc966d
JawadElSayed <jawadelsayed3@gmail.com>
2022-10-17 14:02:33 +0300

    backend > adding review route

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
-const { addJob } = require("../controllers/company.controller");
+const { addJob, reviewApplicants } = require("../controllers/company.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const router = Router();
 
-router.post("/add_job", addJob);
+router.post("/add_job", authMiddleware, addJob);
+router.get("/review_applicants", authMiddleware, reviewApplicants);
 
 module.exports = router;
